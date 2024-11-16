1409d2a00bd0943e920caca2c441e5be5cb6f041
frankgao <1104569705@qq.com>
2024-11-16 14:49:27 +0800

    roman

--- algorithm/src/main/java/Algorithm20241116/Main.java
+++ algorithm/src/main/java/Algorithm20241116/Main.java
@@ -28,29 +28,49 @@ public class Main {
         int result = 0;
         // 遍历字符串
         int i = 0;
-        while (i < s.length() && i + 1 < s.length()) {
+        while (i < s.length()) {
             String current = String.valueOf(s.charAt(i));
-            String next = String.valueOf(s.charAt(i + 1));
+            if (i + 1 < s.length()) {
+            
+                String next = String.valueOf(s.charAt(i + 1));
 
-            String combine = current + next;
-            if (combine.equals(four)) {
-                result += 4;
-                i += 2;
-            } else if (combine.equals(nine)) {
-                result += 9;
-                i += 2;
-            } else if (combine.equals(forty)) {
-                result += 40;
-                i += 2;
-            } else if (combine.equals(ninety)) {
-                result += 90;
-                i += 2;
-            } else if (combine.equals(fourHundred)) {
-                result += 400;
-                i += 2;
-            } else if (combine.equals(nineHundred)) {
-                result += 900;
-                i += 2;
+                String combine = current + next;
+                if (combine.equals(four)) {
+                    result += 4;
+                    i += 2;
+                } else if (combine.equals(nine)) {
+                    result += 9;
+                    i += 2;
+                } else if (combine.equals(forty)) {
+                    result += 40;
+                    i += 2;
+                } else if (combine.equals(ninety)) {
+                    result += 90;
+                    i += 2;
+                } else if (combine.equals(fourHundred)) {
+                    result += 400;
+                    i += 2;
+                } else if (combine.equals(nineHundred)) {
+                    result += 900;
+                    i += 2;
+                } else {
+                    if (current.equals(five)) {
+                        result += 5;
+                    } else if (current.equals(ten)) {
+                        result += 10;
+                    } else if (current.equals(fifty)) {
+                        result += 50;
+                    } else if (current.equals(hundred)) {
+                        result += 100;
+                    } else if (current.equals(fiveHundred)) {
+                        result += 500;
+                    } else if (current.equals(thousand)) {
+                        result += 1000;
+                    } else if (current.equals(one)) {
+                        result += 1;
+                    }
+                    i++;
+                }
             } else {
                 if (current.equals(five)) {
                     result += 5;
@@ -71,30 +91,11 @@ public class Main {
             }
         }
 
-        if (s.length() == 1) {
-            if (s.equals(five)) {
-                result += 5;
-            } else if (s.equals(ten)) {
-                result += 10;
-            } else if (s.equals(fifty)) {
-                result += 50;
-            } else if (s.equals(hundred)) {
-                result += 100;
-            } else if (s.equals(fiveHundred)) {
-                result += 500;
-            } else if (s.equals(thousand)) {
-                result += 1000;
-            } else if (s.equals(one)) {
-                result += 1;
-            }
-        }
-
-
         return result;
     }
 
     public static void main(String[] args) {
         Main main = new Main();
-        System.out.println(main.romanToInt("MCMXCIV"));
+        System.out.println(main.romanToInt("III"));
     }
 }
