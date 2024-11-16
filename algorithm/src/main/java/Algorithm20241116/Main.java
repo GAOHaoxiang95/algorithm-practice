f18d514bbea316354c7b2f70aee1efa0def035fa
frankgao <1104569705@qq.com>
2024-11-16 15:02:51 +0800

    roman

--- algorithm/src/main/java/Algorithm20241116/Main.java
+++ algorithm/src/main/java/Algorithm20241116/Main.java
@@ -9,6 +9,9 @@ package Algorithm20241116;
 // M             1000
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public int romanToInt(String s) {
         String one = "I";
@@ -92,6 +95,30 @@ public class Main {
         }
 
         return result;
+    }
+
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt_official(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
