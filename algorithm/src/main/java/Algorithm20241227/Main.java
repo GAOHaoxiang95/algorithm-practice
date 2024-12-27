d30f5cde6725ed34baa2a9482bd190dcca52cee5
frankgao <1104569705@qq.com>
2024-12-27 09:33:43 +0800

    summaryRanges

--- algorithm/src/main/java/Algorithm20241227/Main.java
+++ algorithm/src/main/java/Algorithm20241227/Main.java
@@ -39,6 +39,27 @@ public class Main {
         return result;
     }
 
+    public List<String> summaryRanges_official(int[] nums) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int low = i;
+            i++;
+            while (i < n && nums[i] == nums[i - 1] + 1) {
+                i++;
+            }
+            int high = i - 1;
+            StringBuffer temp = new StringBuffer(Integer.toString(nums[low]));
+            if (low < high) {
+                temp.append("->");
+                temp.append(Integer.toString(nums[high]));
+            }
+            ret.add(temp.toString());
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
         int[] nums = {0,2,3,4,6,8,9};
