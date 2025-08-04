d5fa1e3ace1b2709b541a11f23cab485fc0e38f4
frankgao <1104569705@qq.com>
2025-08-04 23:15:46 +0800

    minMutation

--- algorithm/src/main/java/Algorithm20250803/Main.java
+++ algorithm/src/main/java/Algorithm20250803/Main.java
@@ -1,7 +1,46 @@
 package Algorithm20250803;
 
+import java.util.Arrays;
+
 public class Main {
     public int minMutation(String startGene, String endGene, String[] bank) {
-        return 0;
+        if (startGene.equals(endGene)) {
+            return 0;
+        }
+
+        if (!Arrays.stream(bank).toList().contains(endGene)) {
+            return -1;
+        }
+        char[] genes = {'A', 'C', 'G', 'T'};
+        int count = 0;
+        do {
+            for (int i = 0; i < startGene.length(); i++) {
+                char c = startGene.charAt(i);
+                String prefixString = startGene.substring(0, i);
+                String suffixString = startGene.substring(i + 1);
+
+                boolean isFound = false;
+                for (char gene : genes) {
+                    if (gene != c) {
+                        String newGene = prefixString + gene + suffixString;
+                        if (Arrays.stream(bank).toList().contains(newGene)) {
+                            isFound = true;
+                            startGene = newGene;
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if (!isFound) {
+                    return -1;
+                }
+            }
+        } while (startGene != endGene);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.minMutation("AACCGGTT", "AACCGGTA", new String[] { "AACCGGTA" }));
     }
 }
